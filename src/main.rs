c02812eb700f35a1eb36c38f9db7cac79e4a709a
Armin <armin.weidtler@web.de>
2025-05-15 12:33:03 +0200

    gpu capture jippie

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod capturer;
 
 fn main() {
     ffmpeg_next::init().unwrap();
-    let clipper= Clipper::new(30, 1280, 720, 120);
+    //ffmpeg_next::log::set_level(ffmpeg_next::log::Level::Debug);
+    //let clipper= Clipper::new(30, 1280, 720, 120);
+    let clipper= Clipper::new(30, 2560, 1440, 120);
 
     clipper.start();
 }
